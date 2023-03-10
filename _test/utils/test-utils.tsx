81f521cab5e2ee7b37d7b5381985377ddc576acb
yoldasahmet <yoldas.ahmet@hotmail.com>
2023-03-10 11:49:55 +0100

    Update test-utils.tsx

--- _test/utils/test-utils.tsx
+++ _test/utils/test-utils.tsx
@@ -1,33 +1,3 @@
-// import {NavigationContainer} from '@react-navigation/native';
-// import {render} from '@testing-library/react-native';
-// import {NativeBaseProvider} from 'native-base';
-// import React from 'react';
-// import {useNavigation} from '@react-navigation/native';
-
-// // must have for native base ui
-// const inset = {
-//   frame: {x: 0, y: 0, width: 0, height: 0},
-//   insets: {top: 0, left: 0, right: 0, bottom: 0},
-// };
-
-// const AllTheProviders = ({children}): JSX.Element => {
-//   return (
-//     <NativeBaseProvider initialWindowMetrics={inset}>
-//       <NavigationContainer>{children}</NavigationContainer>
-//     </NativeBaseProvider>
-//   );
-// };
-
-// const customRender = (ui, options?: any) =>
-//   render(ui, {wrapper: AllTheProviders, ...(options || {})});
-
-// export * from '@testing-library/react-native';
-
-// export {customRender as render};
-
-// export const useNavigationMock = useNavigation as jest.MockedFunction<
-//   typeof useNavigation
-// >;
 
 import React, { ReactElement } from "react";
 import { render, RenderOptions } from "@testing-library/react";
